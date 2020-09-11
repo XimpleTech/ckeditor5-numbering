93d486333083e44e1f502282a5f0cf0e6ec012a3
Douglas Chang <dogchang70@ximple.com.tw>
2020-09-11 16:25:23 +0800

    Add an input of text and a button for numbered list.

--- src/ui.js
+++ src/ui.js
@@ -1,97 +1,60 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import View from '@ckeditor/ckeditor5-ui/src/view';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 import NumberIcon from '../theme/icons/numbering.svg';
+import InputTextView from '@ckeditor/ckeditor5-ui/src/InputText/inputtextview';
 
 /**
  * Handles registering the toggleable button in the 
  * editor's UI so it can be added to the toolbar, with
- * the name `pastePlainText`.
+ * the name `Numbering`.
  */
 export default class NumberingUI extends Plugin {
-    // init() {
-    //     const editor = this.editor;
-    //     const t = editor.t;
-
-    //     // The "placeholder" dropdown must be registered among the UI components of the editor
-    //     // to be displayed in the toolbar.
-    //     editor.ui.componentFactory.add('numbering', locale => {
-    //         // to be table to use the translation function
-    //         const t = view.t;
-
-    //         view.set({
-    //             label: t('Numbering'),
-    //             withText: false,
-    //             tooltip: true,
-    //             isToggleable: true,
-    //             icon: NumberIcon
-    //         });
-
-    //         const command = editor.commands.get('numbering');
-    //         // Callback executed once the button is clicked.
-    //         view.on('execute', () => {
-    //             const theNum = prompt('', command.numbered);
-
-    //             editor.execute('numbering', { "numbering": theNum });
-    //         });
-
-    //         view.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
-
-    //         return view;
-
-    //         //const dropdownView = createDropdown( locale );
-
-    //         // Populate the list in the dropdown with items.
-    //         //addListToDropdown( dropdownView, getDropdownItemsDefinitions( placeholderNames ) );
-
-    //         // dropdownView.buttonView.set( {
-    //         //     // The t() function helps localize the editor. All strings enclosed in t() can be
-    //         //     // translated and change when the language of the editor changes.
-    //         //     label: t( 'Placeholder' ),
-    //         //     tooltip: true,
-    //         //     withText: true
-    //         // } );
-
-    //         // Disable the placeholder button when the command is disabled.
-    //         //dropdownView.bind('isEnabled').to(command);
-
-    //         // Execute the command when the dropdown item is clicked (executed).
-    //         // this.listenTo(dropdownView, 'execute', evt => {
-    //         //     editor.execute('placeholder', { value: evt.source.commandParam });
-    //         //     editor.editing.view.focus();
-    //         // });
-
-    //         // return dropdownView;
-    //     });
-    // }
-
     init() {
         const editor = this.editor;
 
-        editor.ui.componentFactory.add( 'numbering', locale => {
-            const view = new ButtonView( locale );
-            const command = editor.commands.get( 'numbering' );
-
-            // to be table to use the translation function
-            const t = view.t; 
-
-            view.set( {
-                label: t('Numbering'),
-                withText: false,
-                tooltip: true,
-                isToggleable: true,
-                icon:NumberIcon
-            } );
-
-            // Callback executed once the button is clicked.
-            view.on( 'execute', () => {
-                const theNum = prompt('', command.numbered);
-
-                editor.execute('numbering', { "numbering": theNum });
-            } );
-
-            view.bind( 'isOn', 'isEnabled' ).to( command, 'value', 'isEnabled' );
+        editor.ui.componentFactory.add('numbering', locale => {
+            const view = new View(locale);
+            const command = editor.commands.get('numbering');
+            const inputtextView = this._createInputTextView(locale, command);
+            const buttonView = this._createButtonView(locale, command, inputtextView, editor);
+            
+            view.setTemplate({
+                tag: 'div',
+                children: [
+                    inputtextView,
+                    buttonView
+                ]
+            });
 
             return view;
-        } );
+        });
+    }
+
+    _createButtonView(locale, command, inputtextView, editor) {
+        const buttonView = new ButtonView(locale);
+        const t = buttonView.t;
+        buttonView.set({
+            label: t('Numbering'),
+            withText: false,
+            tooltip: true,
+            isToggleable: true,
+            icon: NumberIcon
+        });
+        // Callback executed once the button is clicked.
+        buttonView.on('execute', () => {
+            command.numbered = inputtextView.element.value;
+            editor.execute('numbering', { "numbering": command.numbered });
+        });
+        buttonView.bind('isOn', 'isEnabled').to(command, 'value', 'isEnabled');
+
+        return buttonView;
+    }
+
+    _createInputTextView(locale, command) {
+        const inputtextView = new InputTextView(locale);
+        inputtextView.bind('value').to(command, 'numbered');
+        
+        return inputtextView;
     }
 };
